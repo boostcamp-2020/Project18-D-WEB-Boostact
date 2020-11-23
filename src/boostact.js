c9496d0f41902884b37b730c9c26f142bf96960b
kakasoo <55487286+kakamura@users.noreply.github.com>
2020-11-23 12:34:49 +0900

    [feat] style tag를 인식할 수 있도록 수정 #0

--- src/boostact.js
+++ src/boostact.js
@@ -1,41 +1,46 @@
 function createElement(type, props, ...children) {
-  if (typeof type === "function") {
-    return type(props);
-  }
-  return {
-    type,
-    props: {
-      ...props,
-      children: children.map((child) =>
-        typeof child === "object" ? child : createTextElement(child)
-      ),
-    },
-  };
+    if (typeof type === "function") {
+        return type(props);
+    }
+    return {
+        type,
+        props: {
+            ...props,
+            children: children.map((child) =>
+                typeof child === "object" ? child : createTextElement(child)
+            ),
+        },
+    };
 }
 
 function createTextElement(text) {
-  return {
-    type: "TEXT_ELEMENT",
-    props: {
-      nodeValue: text,
-      children: [],
-    },
-  };
+    return {
+        type: "TEXT_ELEMENT",
+        props: {
+            nodeValue: text,
+            children: [],
+        },
+    };
 }
 
 function render(element, container) {
-  const dom =
-    element.type == "TEXT_ELEMENT"
-      ? document.createTextNode("")
-      : document.createElement(element.type);
-  const isProperty = (key) => key !== "children";
-  Object.keys(element.props)
-    .filter(isProperty)
-    .forEach((name) => {
-      dom[name] = element.props[name];
-    });
-  element.props.children.forEach((child) => render(child, dom));
-  container.appendChild(dom);
+    const dom =
+        element.type == "TEXT_ELEMENT"
+            ? document.createTextNode("")
+            : document.createElement(element.type);
+    const isProperty = (key) => key !== "children";
+    Object.keys(element.props)
+        .filter(isProperty)
+        .forEach((name) => {
+            dom[name] = element.props[name];
+            if (name === "style") {
+                Object.keys(element.props.style).forEach((css) => {
+                    dom.style[css] = element.props.style[css];
+                });
+            }
+        });
+    element.props.children.forEach((child) => render(child, dom));
+    container.appendChild(dom);
 }
 
 export default { render, createElement };
