23f1dbae02d921d7c36957f5883ee5c6a1980174
ji <ji_meng@naver.com>
2020-11-25 14:55:17 +0900

    [chore] 시즌3 시작코드 #0

--- demo/index.js
+++ demo/index.js
@@ -2,5 +2,11 @@ import BoostAct from "../src/boostact";
 import element from "./component";
 
 /** @jsx BoostAct.createElement */
+const element = (
+    <ul>
+        <li>item 1</li>
+        <li>item 2</li>
+    </ul>
+)
 const container = document.getElementById("root");
 BoostAct.render(element, container);
--- src/boostact/Boostact.js
+++ src/boostact/Boostact.js
@@ -1,27 +1,59 @@
-const Boostact = () => {
-  function createTextElement(text) {
-    if (!text) return;
-    return {
-      type: "TEXT_ELEMENT",
-      props: {
-        nodeValue: text,
-        children: [],
-      },
-    };
-  }
-
-  function createElement(type, props, ...children) {
-    console.log(this);
-    return {
-      type,
-      props: {
-        ...props,
-        children: children.map((child) => (typeof child === "object" ? child : this.createTextElement(child))),
-      },
-    };
-  }
-
-  return { createElement, createTextElement };
+const createTextElement = (text) => {
+  return {
+    type: "TEXT_NODE",
+    props: { nodeValue: text },
+    children: [],
+  };
 };
 
-export default Boostact();
+const createElement = (type, props, ...children) => {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) => {
+        if (typeof child !== "object") {
+          createTextElement(child);
+        }
+        return child;
+      }),
+    },
+  };
+};
+
+const vRoot = {}; 
+const workLoop = async () => {
+  if (true) {
+    const result = await makeVDOM();
+    reflectDOM(result);
+  }
+  requestIdleCallback(workLoop);
+};
+
+const render = (component, container) => {
+  vRoot.dom = container;
+  requestIdleCallback(workLoop);
+};
+
+// ppt 5,6  page
+const makeVDOM = (component, vRoot) => {
+  return new Promise( () => {
+
+  })
+};
+
+const dertermineState = () => {};
+const makeVnode = (element) => {};
+// ppt 7 page
+const reflectDOM = () => {};
+
+const useState = (initValue) => {
+  const value = initValue;
+  const state = () => {
+    return value;
+  };
+  const setState = (nextValue) => {};
+  return [state, setState];
+};
+
+export { render, createElement };
