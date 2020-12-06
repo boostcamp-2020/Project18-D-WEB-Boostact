98284887803a05485e9be5737717a4c3cda8e816
kakasoo <55487286+kakamura@users.noreply.github.com>
2020-12-06 17:40:37 +0900

    [chore] 예제 코드 제공 #19

--- demo/App.jsx
+++ demo/App.jsx
@@ -1,12 +1,27 @@
 import Boostact from "../src/boostact/Boostact";
-import Route from "../src/boostact/Route";
-import Router from "./Router";
+import context from "./context";
+import Test from "./test";
 
 /** @jsx Boostact.createElement */
+
 const App = () => {
+  const contextValue = Boostact.useContext(context);
+
   return (
     <div>
-      <Router />
+      <Test />
+      <context.Provider value={2}>
+        <div style={{ border: "1px solid black" }}>
+          <Test />
+          <Test />
+        </div>
+        <context.Provider value={3}>
+          <Test />
+          <Test />
+        </context.Provider>
+        <Test />
+        <Test />
+      </context.Provider>
     </div>
   );
 };
