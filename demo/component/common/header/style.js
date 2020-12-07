bf397fe4d44ce966d14c5edbaedd2fc192e46d19
jaeik <wodlr2007@naver.com>
2020-12-07 23:51:06 +0900

    [Feat] 헤더 구현
    - 버튼 컴포넌트를 사용해서 헤더를 구현했다.

--- demo/component/common/header/style.js
+++ demo/component/common/header/style.js
@@ -1,3 +1,22 @@
 export const container = {
+  background: "#20232a",
   display: "flex",
+  justifyContent: "space-between",
+  height: "60px",
+  padding: "5px 80px",
+  fontSize: "18px",
+  color: "white",
+};
+
+export const headerDiv = {
+  alignItems: "center",
+  display: "flex",
+};
+
+export const contents = {
+  padding: "0px 20px",
+};
+
+export const github = {
+  textDecoration: "none",
 };
