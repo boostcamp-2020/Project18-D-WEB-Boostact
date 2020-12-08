28f77b4d3944be562e7c5c5d06759c65ac048659
jaeik <wodlr2007@naver.com>
2020-12-08 22:04:13 +0900

    [feat] 예제 네비게이터 구현
    - 헤더의 예제 네비게이터를 클릭하면 나오는 네비게이터 구현

--- demo/component/common/navigatorExample/navigator.jsx
+++ demo/component/common/navigatorExample/navigator.jsx
@@ -5,30 +5,18 @@ import { style } from "./style";
 /** @jsx Boostact.createElement */
 
 const Docs = () => {
-  const contentsInstall = [
-    { name: "Hello World", link: "/document" },
-    { name: "Virtual DOM", link: "/tutorial" },
-  ];
-
-  const contentsConcept = [
-    { name: "Hello World", link: "localhost:9000/start" },
-    { name: "Virtual DOM", link: "localhost:9000/gaenyum" },
-    { name: "HOOK 상태 관리", link: "localhost:9000/gaenyum" },
-  ];
-
-  const contentsHooks = [
-    { name: "useState", link: "localhost:9000/start" },
-    { name: "useEffect", link: "localhost:9000/start" },
-    { name: "useContext", link: "localhost:9000/start" },
-    { name: "useReducer", link: "localhost:9000/start" },
-    { name: "useMemo", link: "localhost:9000/start" },
+  const contentsExample = [
+    { name: "useState", link: "/useState" },
+    { name: "useEffect", link: "/useEffect" },
+    { name: "useReducer", link: "/useReducer" },
+    { name: "useContext", link: "/useContext" },
+    { name: "useMemo", link: "/useMemo" },
+    { name: "useCallback", link: "/useCallback" },
   ];
 
   return (
     <div style={style.navContainer}>
-      <NavContainer title="설치하기" lists={contentsInstall} />
-      <NavContainer title="주요 개념" lists={contentsConcept} />
-      <NavContainer title="HOOKS" lists={contentsHooks} />
+      <NavContainer title="예제" lists={contentsExample} opened={true} />
     </div>
   );
 };
