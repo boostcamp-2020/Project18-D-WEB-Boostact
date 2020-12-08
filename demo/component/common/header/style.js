f79cd1c73f2546d6d38b3bd7bc5b6cf8661d78b9
jaeik <wodlr2007@naver.com>
2020-12-08 21:56:36 +0900

    [fix] 헤더 레이아웃 잡기

--- demo/component/common/header/style.js
+++ demo/component/common/header/style.js
@@ -6,16 +6,15 @@ export const container = {
   padding: "0px 80px",
   fontSize: "18px",
   color: "white",
+  minWidth: "70%",
+  paddingLeft: `calc(15% - 20px)`,
+  paddingRight: "15%",
 };
 
 export const headerDiv = {
   alignItems: "center",
   display: "flex",
   font: "16px",
-};
-
-export const contents = {
-  padding: "0px 20px",
 };
 
 export const github = {
