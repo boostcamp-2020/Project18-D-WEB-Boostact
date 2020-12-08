9acbc51d55ef33ef502b7c827aaa6ed81abb5b44
Seunghyo <seunghyoku@gmail.com>
2020-12-08 17:48:14 +0900

    [chore] footer의 A 컴포넌트 common 폴더로 이동 #40 - 바꾼 경로에 맞게 import 경로 수정 및 style 객체 변경

--- demo/component/common/a/a.jsx
+++ demo/component/common/a/a.jsx
@@ -1,13 +1,13 @@
 import Boostact from "../../../../src/boostact/Boostact";
 import Route from "../../../../src/boostact/Route";
-import { style } from "./style";
+import { a } from "./style";
 
 /** @jsx Boostact.createElement */
 
 const A = (props) => {
   const { link, name } = props;
   return (
-    <a href={link} style={style.a} onMouseOver={(e) => (e.target.style.color = "#61dafb")} onMouseOut={(e) => (e.target.style.color = "white")}>
+    <a href={link} style={a} onMouseOver={(e) => (e.target.style.color = "#61dafb")} onMouseOut={(e) => (e.target.style.color = "white")}>
       {name}
     </a>
   );
