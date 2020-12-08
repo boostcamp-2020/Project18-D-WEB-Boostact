b7a5117620c8a63d93c973d72c3492c74379c1c1
kakasoo <55487286+kakamura@users.noreply.github.com>
2020-12-08 10:20:22 +0900

    [chore] npm module의 entry point 수정 #0

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 import Boostact from "./src/boostact/Boostact.js";
 /** @jsx Boostact.createElement */
 
-console.log("@@@@@@@@@@@@@@@@@");
-console.log("boostact working.");
-console.log(Boostact);
-console.log("@@@@@@@@@@@@@@@@@");
 export default Boostact;
