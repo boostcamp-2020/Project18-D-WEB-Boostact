cf155d19c0171431b4a5691697907b50937db871
kakasoo <55487286+kakamura@users.noreply.github.com>
2020-12-09 17:06:50 +0900

    [chore] 코드 공유 #62

--- demo/page/bugReport/style.js
+++ demo/page/bugReport/style.js
@@ -6,6 +6,9 @@ jss.setup(preset());
 const styleSheet = {
   developReport: { paddingLeft: "30%", paddingRight: "30%" },
   date: {
+    fontSize: "20px",
+    fontWeight: "1000",
+    color: "#61DAFB",
     marginBottom: "0px",
     marginTop: "0px",
     padding: "0px",
