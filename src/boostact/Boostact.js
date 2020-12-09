d20457a796a14dad7df9a15a8b830323b223d9f9
ji <ji_meng@naver.com>
2020-12-09 22:04:58 +0900

    [fix] 비교하는 돔중에 한쪽에만 style이 있는 경우 발생하는 에러 수정

--- src/boostact/Boostact.js
+++ src/boostact/Boostact.js
@@ -172,7 +172,7 @@ const shallowEqual = (object1, object2) => {
 
   for (const key of keys1) {
     if (object1[key] && key === "style") {
-      if (!shallowEqual(object1[key], object2[key])) {
+      if (!object2[key] && !shallowEqual(object1[key], object2[key])) {
         return false;
       }
     } else if (object1[key] !== object2[key]) {
