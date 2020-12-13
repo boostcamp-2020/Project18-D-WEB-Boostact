9bfedff90fd82e92f49bba3c821aaf27ce7a8852
kakasoo <55487286+kakamura@users.noreply.github.com>
2020-12-13 16:38:48 +0900

    [chore] HOOK 설명 페이지 제작 #70

--- demo/page/document/index/helloWorld.jsx
+++ demo/page/document/index/helloWorld.jsx
@@ -20,51 +20,141 @@ Boostact는 그 결과로써 탄생한 프레임워크입니다. Boostact는 Rea
 ​
 Boostact를 사용하기 위해서는 webpack과 babel이 필수적으로 필요합니다. 따라서 아래의 devDependencies를 모두 추가해주시기 바랍니다.
 ​    
-### 웹팩 설치
+### 웹팩 설치 및 바벨 설치
 `);
 
 const highlight1 = hljs.highlight(
   "javascript",
   `
-const useState = (initValue) => {
-  HOOKS[HOOK_ID] = HOOKS[HOOK_ID] || initValue;
-  const CURRENT_HOOK_ID = HOOK_ID++;
-
-  const setState = (nextValue) => {
-    typeof nextValue === "function" ?
-    (HOOKS[CURRENT_HOOK_ID] = nextValue(HOOKS[CURRENT_HOOK_ID])) : 
-    (HOOKS[CURRENT_HOOK_ID] = nextValue);
-    reRender();
-  };
-  return [HOOKS[CURRENT_HOOK_ID], setState];
-};
+npm install webpack webpack-cli webpack-dev-server --save-dev
+npm install html-webpack-plugin mini-css-extract-plugin --save-dev
+npm install @babel/cli @babel/core @babel/polyfill @babel/preset-env @babel/preset-react --save-dev
 `
 ).value;
 
 const string2 = marked(`
-### 바벨 설치
 
 웹팩과 바벨을 설치한 후에는 아래와 같은 setting file들이 필요합니다. 다만 아래는 예시일 뿐이니, 필요에 따라 다른 설정을 추가하셔도 좋습니다.
 ​​
 ### .babelrc`);
 
+const highlight2 = hljs.highlight(
+  "javascript",
+  `
+{
+  "presets": ["@babel/preset-env", "@babel/preset-react"]
+}
+`
+).value;
+
 const string3 = marked(`
 ### webpack.config.js
 `);
 
-const string4 = marked(`설정이 모두 끝났다면, 이제 package.json에서 webpack 실행 명령어를 만들어주시면 됩니다. 다음으로는 boostact의 동작을 확인할 수 있는 코드들입니다.
+const highlight3 = hljs.highlight(
+  "javascript",
+  `
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+module.exports = {
+    mode: "development",
+    entry: {
+        index: ["@babel/polyfill", "./index.js"],
+    },
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\\.(js|jsx)$/,
+                use: "babel-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    devServer: {
+        host: "127.0.0.1",
+        contentBase: path.join(__dirname, "/dist"),
+        compress: true,
+        hot: true,
+        inline: true,
+        port: 9000,
+        open: true,
+    },
+    resolve: {
+        extensions: [".js", ".jsx"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "index",
+            hash: true,
+            chunks: ["index"],
+            filename: "index.html",
+            template: "./index.html",
+        }),
+    ],
+};
+    `
+).value;
+
+const string4 = marked(`설정이 모두 끝났다면, 이제 package.json에서 webpack 실행 명령어를 만들어주시면 됩니다. 다음으로는 boostact의 동작을 확인할 수 있는 예제 코드들입니다.
 
 ### index.html
 `);
+
+const highlight4 = hljs.highlight(
+  "javascript",
+  `
+<!DOCTYPE html>
+<body>
+    <div id="root"></div>
+</body>
+<script type="module" src="index.jsx"></script>
+`
+).value;
+
 const string5 = marked(`### index.js`);
 
+const highlight5 = hljs.highlight(
+  "javascript",
+  `
+import Boostact from "boostact";
+import App from "./App";
+/** @jsx Boostact.createElement */
+const root = document.getElementById("root");
+Boostact.render(<App />, root);
+`
+).value;
+
 const string6 = marked(`### App.js`);
+
+const highlight6 = hljs.highlight(
+  "javascript",
+  `
+import Boostact from "boostact";
+/** @jsx Boostact.createElement */
+const App = () => {
+    return (
+        <div>
+            <h1>Hello!</h1>
+            <h2>This is Boostact!</h2>
+        </div>
+    );
+};
+export default App;
+`
+).value;
+
 const string7 = marked(`
 여기까지로 모든 설명이 끝났습니다. 주의할 점은, index.js와 App.js file과 같이 jsx 문법을 사용하는 파일들에 다음과 같은 주석을 추가해야 한다는 점입니다.
 이는 babel이 jsx를 React.createElement가 아닌 Boostact.createElement로 처리하게 해줍니다.   
 ​  
 ### jsdoc (essential)
 `);
+
+const highlight7 = hljs.highlight("javascript", `/** @jsx Boostact.createElement/`).value;
 
 const HelloWorld = () => {
   return (
@@ -79,11 +169,58 @@ const HelloWorld = () => {
       </pre>
 
       <div className={classes.explaination} dangerouslySetInnerHTML={string2} />
+      <pre className={classes.codeBox}>
+        <code
+          className="javascript hljs"
+          dangerouslySetInnerHTML={highlight2}
+          style={{ marginRight: "20px", color: "white", borderRadius: "30px" }}
+        ></code>
+      </pre>
+
       <div className={classes.explaination} dangerouslySetInnerHTML={string3} />
+      <pre className={classes.codeBox}>
+        <code
+          className="javascript hljs"
+          dangerouslySetInnerHTML={highlight3}
+          style={{ marginRight: "20px", color: "white", borderRadius: "30px" }}
+        ></code>
+      </pre>
+
       <div className={classes.explaination} dangerouslySetInnerHTML={string4} />
+      <pre className={classes.codeBox}>
+        <code
+          className="javascript hljs"
+          dangerouslySetInnerHTML={highlight4}
+          style={{ marginRight: "20px", color: "white", borderRadius: "30px" }}
+        ></code>
+      </pre>
+
       <div className={classes.explaination} dangerouslySetInnerHTML={string5} />
+      <pre className={classes.codeBox}>
+        <code
+          className="javascript hljs"
+          dangerouslySetInnerHTML={highlight5}
+          style={{ marginRight: "20px", color: "white", borderRadius: "30px" }}
+        ></code>
+      </pre>
+
       <div className={classes.explaination} dangerouslySetInnerHTML={string6} />
+      <pre className={classes.codeBox}>
+        <code
+          className="javascript hljs"
+          dangerouslySetInnerHTML={highlight6}
+          style={{ marginRight: "20px", color: "white", borderRadius: "30px" }}
+        ></code>
+      </pre>
+
       <div className={classes.explaination} dangerouslySetInnerHTML={string7} />
+      <pre className={classes.codeBox}>
+        <code
+          className="javascript hljs"
+          dangerouslySetInnerHTML={highlight7}
+          style={{ marginRight: "20px", color: "white", borderRadius: "30px" }}
+        ></code>
+      </pre>
     </div>
   );
 };
--- demo/page/document/index/useContext.jsx
+++ demo/page/document/index/useContext.jsx
@@ -1,13 +1,169 @@
 import Boostact from "../../../../src/boostact/Boostact";
-
+import hljs from "highlight.js/lib/core";
+import javascript from "highlight.js/lib/languages/javascript";
+hljs.registerLanguage("javascript", javascript);
+import classes from "../style";
 /** @jsx Boostact.createElement */
 
-const string = marked(`
-# Hello world4
+const string1 = marked(`
+# useContext
+
+jsx에서 하위 컴포넌트로 값을 전달하는 것은, 그 깊이가 깊어질 수록 더 힘들어질 수 밖에 없습니다.  
+React에서는 이걸 해결하기 위해 useContext를 만들었고, Boostact 역시 useContext 기능을 구현해놓았습니다.  
+useContext를 사용하면 값을 props를 통해 전달할 필요없이 전역에서 사용할 수 있습니다.   
+아래는 useContext와 createContext의 전체 코드입니다.
 `);
 
-const hooksState4 = () => {
-  return <div>asd4</div>;
+const highlight1 = hljs.highlight(
+  "javascript",
+  `
+const useContext = (context) => {
+  if (!context) {
+    throw new Error("Parameter is nothing. (ex. useContext(context))");
+  }
+  if (context.id === null || context.id === undefined) {
+    throw new Error("Maybe it's not context... Because it doesn't have context.\"id\"");
+  }
+  return currentContext[context.id];
 };
 
-export default hooksState4;
+const createContext = (defaultValue) => {
+  const CURRENT_CONTEXT_ID = CONTEXT_ID++;
+  defaultContext[CURRENT_CONTEXT_ID] = defaultValue;
+  currentContext = defaultContext;
+  const useContextHook = {
+    id: CURRENT_CONTEXT_ID,
+    Provider: (props) => {
+      const newContext = [...currentContext];
+      newContext[CURRENT_CONTEXT_ID] = props.value;
+      props.children.forEach((child) => {
+        child.context = newContext;
+      });
+      return { type: "CONTEXT", props: { children: props.children } };
+    },
+  };
+
+  CONTEXTS[CURRENT_CONTEXT_ID] = CONTEXTS[CURRENT_CONTEXT_ID] || useContextHook;
+  return CONTEXTS[CURRENT_CONTEXT_ID];
+};
+`
+).value;
+
+const string2 = marked(`
+여기서도 마찬가지로, 에러 처리를 제외하고 하나 씩 분석해보도록 하겠습니다.
+`);
+
+const highlight2 = hljs.highlight(
+  "javascript",
+  `
+const useContext = (context) => {
+  return currentContext[context.id];
+};
+`
+).value;
+
+const string3 = marked(`
+useContext는 간단합니다. context를 매개변수로 받아서 currentContext 안에서 id 값에 해당하는 값을 돌려줍니다.  
+**currentContext는 useContext 기능만을 위해 만들어진 별도의 HOOKS 배열**입니다.  
+useContext는 어려운 게 없기 때문에, context가 어떻게 만들어지고 들어오는가를 볼 필요가 있어 보입니다.
+`);
+
+const highlight3 = hljs.highlight(
+  "javascript",
+  `
+const createContext = (defaultValue) => {
+  const CURRENT_CONTEXT_ID = CONTEXT_ID++;
+  defaultContext[CURRENT_CONTEXT_ID] = defaultValue;
+  currentContext = defaultContext;
+  const useContextHook = {
+    id: CURRENT_CONTEXT_ID,
+    Provider: (props) => {
+      const newContext = [...currentContext];
+      newContext[CURRENT_CONTEXT_ID] = props.value;
+      props.children.forEach((child) => {
+        child.context = newContext;
+      });
+      return { type: "CONTEXT", props: { children: props.children } };
+    },
+  };
+
+  CONTEXTS[CURRENT_CONTEXT_ID] = CONTEXTS[CURRENT_CONTEXT_ID] || useContextHook;
+  return CONTEXTS[CURRENT_CONTEXT_ID];
+};
+`
+).value;
+
+const string4 = marked(`
+createContext는 context를 만드는 함수입니다. 위에부터 차례대로 보도록 하겠습니다.  
+먼저 다른 HOOK과 같이 인덱스를 추적하기 위해 CURRENT_CONTEXT_ID를 만듭니다.  
+우선적으로 defaultContext[CURRENT_CONTEXT_ID]에 디폴트 값을 저장해두고, 그것을 현재 Context에도 대입해둡니다.
+`);
+
+const highlight4 = hljs.highlight(
+  "javascript",
+  `
+const useContextHook = {
+  id: CURRENT_CONTEXT_ID,
+  Provider: (props) => {
+    const newContext = [...currentContext];
+    newContext[CURRENT_CONTEXT_ID] = props.value;
+    props.children.forEach((child) => {
+      child.context = newContext;
+    });
+    return { type: "CONTEXT", props: { children: props.children } };
+  },
+};
+`
+).value;
+
+const string5 = marked(`
+내부의 useContextHook은 결과적으로 만들고자 하는 context입니다.  
+context를 탐색하기 위한 id 값, 디폴트 값 이후로도 값을 재정의하기 위한 Provider 함수로 이루어져 있습니다.
+Provider 함수는 새 Context를 만들어 props의 value를 저장하고, 자식들의 모든 context를 newContext로 바꾸는 함수입니다.
+결과적으로 Provider는 CONTEXT를 타입으로 갖는 JS 객체를 만듭니다. (이는 렌더링되는 Element가 아닙니다.)
+    
+다시 createContext 함수를 보면, 결과적으로 이미 있는 경우와 첫 생성되는 경우를 파악해 CONTEXTS를 저장하는 것을 볼 수 있습니다.
+createContext는 이렇게 만들어진 CONTEXT[CURRENT_CONTEXT_ID]를 반환합니다.
+`);
+
+const ArticleOfUseContext = () => {
+  return (
+    <div className={classes.paddingRightAndBottom}>
+      <div className={classes.explaination} dangerouslySetInnerHTML={string1} />
+      <pre className={classes.codeBox}>
+        <code
+          className="javascript hljs"
+          dangerouslySetInnerHTML={highlight1}
+          style={{ marginRight: "20px", color: "white", borderRadius: "30px" }}
+        ></code>
+      </pre>
+      <div className={classes.explaination} dangerouslySetInnerHTML={string2} />
+      <pre className={classes.codeBox}>
+        <code
+          className="javascript hljs"
+          dangerouslySetInnerHTML={highlight2}
+          style={{ marginRight: "20px", color: "white", borderRadius: "30px" }}
+        ></code>
+      </pre>
+      <div className={classes.explaination} dangerouslySetInnerHTML={string3} />
+      <pre className={classes.codeBox}>
+        <code
+          className="javascript hljs"
+          dangerouslySetInnerHTML={highlight3}
+          style={{ marginRight: "20px", color: "white", borderRadius: "30px" }}
+        ></code>
+      </pre>
+      <div className={classes.explaination} dangerouslySetInnerHTML={string4} />
+      <pre className={classes.codeBox}>
+        <code
+          className="javascript hljs"
+          dangerouslySetInnerHTML={highlight4}
+          style={{ marginRight: "20px", color: "white", borderRadius: "30px" }}
+        ></code>
+      </pre>
+      <div className={classes.explaination} dangerouslySetInnerHTML={string5} />
+    </div>
+  );
+};
+
+export default ArticleOfUseContext;
